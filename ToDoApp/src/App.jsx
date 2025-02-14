f88557ebde7aa9ad5ece620f12e9a9c58b930190
Vineeth Narmala <n.vineeth1704@gmail.com>
2025-02-14 17:36:39 -0500

    updated read me

---
+++ ToDoApp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+   return(
+   <>
+   <h2 className='text-3xl underline text-center'>ToDo App Using React</h2>
+    </>
+  )
+}
+
+export default App
